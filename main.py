a76bedfef5fd93afd9d0c85c589eccb00e238a7f
Elliot Colp <thelastbanana@gmail.com>
2013-03-27 10:30:56 -0600

    Added a basic map to demonstrate file loading. Also resized the screen to 800x600, and tiles to 20x20.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys, pygame, tiles
 from gui import GUI
 
 pygame.init()
-main_gui = GUI(pygame.Rect(0, 0, 640, 480), (0, 0, 0))
+main_gui = GUI(pygame.Rect(0, 0, 800, 600), (0, 0, 0))
 clock = pygame.time.Clock()
     
-test_map = tiles.TileMap(main_gui.screen, "assets/tiles.png", 24, 24, 20, 20)
+test_map = tiles.TileMap(main_gui.screen, "assets/tiles.png", 20, 20, 30, 30)
+test_map.load_from_file("maps/basic.map")
 main_gui.add(test_map)
 
 while 1:
