f7fa8619d33e7fab980c36654c9870dd1a8595b0
kuzi117 <puckmaster_84@hotmail.com>
2013-03-28 13:56:55 -0600

    Added a quit on q press clause. Also fixed some funny indentation errors (ie tabs).

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from gui import GUI
 pygame.init()
 main_gui = GUI(pygame.Rect(0, 0, 800, 600), (0, 0, 0))
 clock = pygame.time.Clock()
-    
-test_map = tiles.TileMap(main_gui.screen, "assets/tiles.png", 20, 20, 30, 30)
-test_map.load_from_file("maps/basic.map")
-main_gui.add(test_map)
 
 while 1:
     for event in pygame.event.get():
-	    if event.type == pygame.QUIT:
-		    pygame.display.quit()
-		    sys.exit()
-
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+        #end if q is pressed
+        elif event.type == pygame.KEYDOWN and event.unicode == 'q':
+            pygame.display.quit()
+            sys.exit()
     main_gui.draw()
     clock.tick(60)
