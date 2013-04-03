5b5de23a6900c830b69c2d56be803ba3f76aa11a
kuzi117 <puckmaster_84@hotmail.com>
2013-04-03 15:38:17 -0600

    Added movement function.

--- unit/base_unit.py
+++ unit/base_unit.py
@@ -1,5 +1,6 @@
 import pygame
 import unit
+import helper
 from pygame.sprite import Sprite
 
 class BaseUnit(Sprite):
@@ -16,9 +17,58 @@ class BaseUnit(Sprite):
     
         Sprite.__init__(self)
         
+        self._path = []
+
         self.image = self._base_image
         self.rect = self.image.get_rect()
 
+    def update(self):
+        """
+        Overrides the update function.
+        Handles movement.
+        """
+        if self._moving:
+            #checks if path is empty
+            if not self._path:
+                #notify not moving
+                self._moving = False
+                return
+
+            #There's a path to move on
+            else:
+                #If we're at the next tile remove it
+                if self.rect.topleft == self._path[0]:
+                    self._path.pop(0)
+
+                #get values for calcs
+                path_x, path_y = self._path[0]
+                rect_x, rect_y = self.rect.topleft
+
+                #determine deltas
+                dx = helper.clamp(path_x - rect_x, -3, 3)
+                dy = helper.clamp(path_y - rect_y, -3, 3)
+
+                #set the new value
+                new_val = (rect_x + dx, rect_y + dy)
+                self.rect.topleft = new_val
+                
+
+    def set_path(self, path):
+        """
+        Tells the unit that it should be moving.
+        """
+        #if it's an empty path ignore it.
+        if not path: return
+
+        #notify that we're moving
+        self._moving = True
+
+        #get rid of the starting position.
+        path.pop(0)
+
+        #set the path
+        self._path = path
+        
     def is_moving(self):
         return self._moving
         
