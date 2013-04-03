83990c2e9560299cf82e9c3cf8a66395a11f9137
Elliot Colp <thelastbanana@gmail.com>
2013-04-03 11:24:58 -0600

    Never mind about that whole event manager thing.

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import sys, pygame, tiles
 from gui import GUI
-from events import EventManager
 import unit.base_unit
 
-# Initialize pygame
+# Initialize everything
 pygame.init()
-
-# Initialize the GUI
 main_gui = GUI(pygame.Rect(0, 0, 800, 600), (0, 0, 0))
 main_gui.load_level("maps/basic.lvl")
-
-# Initialize the event manager
-event_man = EventManager()
-
-# Initialize the clock
 clock = pygame.time.Clock()
 
 # The main game loop
 while 1:
-    # Get new events
-    event_man.update()
-    
-    # Quit when the window is closed or Q/ESC is pressed.
-    if (event_man.check_event(pygame.QUIT)
-    or event_man.is_key_down(pygame.K_ESCAPE)
-    or event_man.is_key_down(pygame.K_q)):
-        pygame.display.quit()
-        sys.exit()
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+        # End if q is pressed
+        elif event.type == pygame.KEYDOWN and event.unicode == 'q':
+            pygame.display.quit()
+            sys.exit()
+        # Respond to clicks
+        elif event.type == pygame.MOUSEBUTTONUP:
+            main_gui.on_click(event)
     main_gui.draw()
     clock.tick(60)
