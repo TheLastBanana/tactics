7eb4f988fb820d4fe3d1a9c1b6d8e56272133f48
kuzi117 <puckmaster_84@hotmail.com>
2013-04-05 10:33:32 -0600

    Added some docs. Added move_cost and is_passable. Added facings when moving. Changed movement speeds to be tile coord based.

--- unit/base_unit.py
+++ unit/base_unit.py
@@ -3,6 +3,8 @@ import unit
 import helper
 from pygame.sprite import Sprite
 
+FRAME_MOVE_SPEED = 3/20
+
 class BaseUnit(Sprite):
     """
     The basic representation of a unit from which all other unit types
@@ -24,7 +26,7 @@ class BaseUnit(Sprite):
 
     def update(self):
         """
-        Overrides the update function.
+        Overrides the update function of the Sprite class.
         Handles movement.
         """
         if self._moving:
@@ -45,8 +47,25 @@ class BaseUnit(Sprite):
                 rect_x, rect_y = self.rect.topleft
 
                 #determine deltas
-                dx = helper.clamp(path_x - rect_x, -3, 3)
-                dy = helper.clamp(path_y - rect_y, -3, 3)
+                dx = helper.clamp(path_x - rect_x,
+                                  -FRAME_MOVE_SPEED,
+                                  FRAME_MOVE_SPEED)
+                dy = helper.clamp(path_y - rect_y,
+                                  -FRAME_MOVE_SPEED,
+                                  FRAME_MOVE_SPEED)
+                
+                #by definition, units can only move horizontally or
+                #vertically, never diagonally, therefore if the unit
+                #is still moving then only one of these conditions
+                #will ever occur
+                if dx > 0:
+                    self.set_angle(0)
+                elif dy < 0:
+                    self.set_angle(90)
+                elif dx < 0:
+                    self.set_angle(180)
+                elif dy > 0:
+                    self.set_angle(270)
 
                 #set the new value
                 new_val = (rect_x + dx, rect_y + dy)
@@ -55,7 +74,7 @@ class BaseUnit(Sprite):
 
     def set_path(self, path):
         """
-        Tells the unit that it should be moving.
+        Tells the unit that it should be moving, where, and how.
         """
         #if it's an empty path ignore it.
         if not path: return
@@ -69,7 +88,26 @@ class BaseUnit(Sprite):
         #set the path
         self._path = path
         
+    def move_cost(self, tile):
+        """
+        Returns the cost of a unit moving over a certain tile.
+        
+        Must be implemented by subclasses.
+        """
+        pass
+        
+    def is_passable(self, tile):
+        """
+        Returns whether or not a unit can move over a certain tile.
+        
+        Must be implemented by subclasses.
+        """
+        pass
+        
     def is_moving(self):
+        """
+        Returns whether or not a unit is currently in transit.
+        """
         return self._moving
         
     def set_angle(self, angle):
