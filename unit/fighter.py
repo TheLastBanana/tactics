17dc14636f31721eb9224a4f49d7ce9da4f29be1
TheLastBanana <thelastbanana@gmail.com>
2013-04-11 12:13:14 -0600

    Added bonus damage to fighters against other air units.

--- unit/fighter.py
+++ unit/fighter.py
@@ -5,7 +5,8 @@ import pygame
 
 class Fighter(AirUnit):
     """
-    A fighter jet. Moves fast, but needs to refuel constantly.
+    A fighter jet. Moves fast, but needs to refuel constantly. Good at peppering
+    the ground with shots as well as eliminating other air units.
     
     Armour: High
     Speed: Very High
@@ -17,6 +18,7 @@ class Fighter(AirUnit):
     - In order to maintain its high speed, the fighter has fairly low fuel.
       Make well-planned strafing runs and be sure you can get back to a carrier
       in time!
+    - When firing at another air unit, this unit does extra damage.
     """
     sprite = pygame.image.load("assets/fighter.png")
     
@@ -33,9 +35,32 @@ class Fighter(AirUnit):
         self.max_atk_range = 4
         self.damage = 5
         self.defense = 3
+        self.bonus_damage = 2
         self.max_fuel = 7
         self.set_fuel(self.max_fuel)
         self.min_move_distance = 6
         self.hit_effect = effects.Ricochet
+        
+    def get_damage(self, target, target_tile):
+        """
+        Returns the potential attack damage against a given enemy.
+        
+        This overrides the super class function to allow
+        special damage effects.
+        """
+        # Do bonus damage to other air units
+        if isinstance(target, unit.air_unit.AirUnit):
+            # Calculate the total damage
+            damage = self.damage + self.bonus_damage
+            
+            # Get the unit's current defense
+            defense = target.get_defense(tile = target_tile)
+            
+            # Don't do negative damage
+            if (damage - defense < 0):
+                return 0
+            
+            return damage - defense
+        else: return super().get_damage(target, target_tile)
 
 unit.unit_types["Fighter"] = Fighter
